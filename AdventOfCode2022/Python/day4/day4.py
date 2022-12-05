f93e35876c8a6d94d9c7b1dfb3311380b5c6d9e6
cu <harrierraum@hotmail.com>
2022-12-05 12:47:44 +0100

    solve day4 task

---
+++ AdventOfCode2022/Python/day4/day4.py
@@ -0,0 +1,30 @@
+file = open('day4.txt')
+total_task_1 = 0
+total_task_2 = 0
+
+while True:
+  line = file.readline()
+  if not line:
+    break
+
+  first_area = [int(i) for i in line.split(",")[0].split("-")]
+  second_area = [int(i) for i in line.split(",")[1].split("-")]
+
+  for place in range(first_area[0]+1, first_area[1]):
+    first_area.append(place)
+
+  for place in range(second_area[0]+1, second_area[1]):
+    second_area.append(place)
+
+  if set(second_area).issubset(set(first_area)) or set(first_area).issubset(second_area):
+    total_task_1 += 1
+
+  for place in first_area:
+    if place in second_area:
+      total_task_2 += 1
+      break
+
+print(total_task_1)
+print(total_task_2)
+
+file.close()
