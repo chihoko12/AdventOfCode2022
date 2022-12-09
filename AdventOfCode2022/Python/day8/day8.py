d441bd5fa1b130070d3ebe289764fe412477b666
cu <harrierraum@hotmail.com>
2022-12-09 15:30:40 +0100

    fix

--- AdventOfCode2022/Python/day8/day8.py
+++ AdventOfCode2022/Python/day8/day8.py
@@ -1,60 +1,36 @@
-f = open('day8_ex.txt')
-grid = [[int(i) for i in line if i.isnumeric()]
-        for line in f.read().split('\n')]
-row_len = len(grid)
-column_len = len(grid[0])
+import numpy as np
 
-visible = 0
-scenic_scores = []
-for row_idx, row in enumerate(grid):
-    for column_idx, tree in enumerate(row):
-        if row_idx == 0 or row_idx == row_len-1:
-            visible += 1
-        elif column_idx == 0 or column_idx == column_len-1:
-            visible += 1
-        else:
-            # Part 1
-            left = grid[row_idx][0:column_idx][::-1]
-            right = grid[row_idx][column_idx + 1:column_len]
-            top = [grid[j][column_idx] for j in range(0, row_idx)][::-1]
-            bottom = [grid[j][column_idx] for j in range(row_idx + 1, row_len)]
+with open('day8.txt') as f:
+    forest = np.array([[int(x) for x in list(line.strip())] for line in f])
 
-            left_bol = all(tree > i for i in left)
-            right_bol = all(tree > i for i in right)
-            top_bol = all(tree > i for i in top)
-            bottom_bol = all(tree > i for i in bottom)
 
-            if any([left_bol, right_bol, top_bol, bottom_bol]):
-                visible += 1
+def look_along(x):
+    return x > np.hstack((-1, np.maximum.accumulate(x)[:-1]))
 
-            # Part 2
-            left_count, right_count, top_count, bottom_count = 0, 0, 0, 0
-            for i in left:
-                if tree > i:
-                    left_count += 1
-                else:
-                    left_count += 1
-                    break
-            for i in right:
-                if tree > i:
-                    right_count += 1
-                else:
-                    right_count += 1
-                    break
-            for i in top:
-                if tree > i:
-                    top_count += 1
-                else:
-                    top_count += 1
-                    break
-            for i in bottom:
-                if tree > i:
-                    bottom_count += 1
-                else:
-                    bottom_count += 1
-                    break
-            scenic_score = left_count * right_count * bottom_count * top_count
-            scenic_scores.append(scenic_score)
 
-print(visible)
-print(max(scenic_scores))
+is_visible = np.apply_along_axis(look_along, 0, forest)
+is_visible |= np.apply_along_axis(look_along, 1, forest)
+is_visible |= np.apply_along_axis(look_along, 0, forest[::-1, :])[::-1, :]
+is_visible |= np.apply_along_axis(look_along, 1, forest[:, ::-1])[:, ::-1]
+print('Day 8, part 1:', is_visible.sum())
+
+
+def compute_scenic_score(candidate_tree):
+    height = forest[candidate_tree]
+    row, col = candidate_tree
+    if row == 0 or col == 0 or row == forest.shape[0] - 1 or col == forest.shape[1] - 1:
+        return 0
+
+    score = (np.maximum.accumulate(forest[row, col + 1:-1]) < height).sum() + 1
+    score *= (np.maximum.accumulate(forest[row +
+              1:-1, col]) < height).sum() + 1
+    score *= (np.maximum.accumulate(forest[row,
+              col - 1:0:-1]) < height).sum() + 1
+    score *= (np.maximum.accumulate(forest[row -
+              1:0:-1, col]) < height).sum() + 1
+    return score
+
+
+scenic_scores = [compute_scenic_score(tree)
+                 for tree in zip(*np.nonzero(is_visible))]
+print('Day 8, part 2:', np.max(scenic_scores))
